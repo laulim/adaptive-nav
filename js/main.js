0a88338eed6537fc7757fb42fdb64e870fdcc5bf
Mila <laudylim@yandex.ru>
2018-04-02 22:06:41 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
+//Создает переменные для кнопки и для меню
+	var pull = $("#pull");
+	var nav = $("#mobile-nav");
 
-	
-	/* Call plugins hrere ...*/
+	//Описываем событие при нажатии на кнопку
+	$(pull).on("click", function(e){
 
-	
+		// Отменяем стандартное поведение ссылки в браузере
+		e.preventDefault();
+
+		// Открываем/скрываем меню
+		$(nav).slideToggle();
+	});
+
+	// При изменении размера окна в большую сторону, если меню было скрыто, показываем его
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 720 && nav.is(':hidden')) {
+			nav.removeAttr('style');
+		}
+	});
 });
